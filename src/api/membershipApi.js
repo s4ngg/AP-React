5ec05f9049d5ec75c1eca33a637b1488f8d5bd1c
s4ngg <s4ngg@naver.com>
2026-05-12 12:08:36 +0900

    fix: 다음 멤버십 등급까지 얼마를 결제해야하는지 저장하는 api등록

--- src/api/membershipApi.js
+++ src/api/membershipApi.js
@@ -2,4 +2,7 @@ import api from "./index"
 
 // 등급 변경 이력 조회 (PathVariable)
 export const getMembershipHistory = (memberId) =>
-  api.get(`/api/membership/history/${memberId}`).then((res) => res.data.data)
+  api.get(`/api/membership/history/${memberId}`).then((res) => res.data.data)3
+
+export const getMembershipStatus = () =>
+  api.get("/api/membership/status").then((res) => res.data.data)
